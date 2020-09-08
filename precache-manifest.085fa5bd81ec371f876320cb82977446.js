35e77710918743cd23bbc540513967e97ec8da90
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-08 15:28:29 +0530

    Updates

---
+++ precache-manifest.085fa5bd81ec371f876320cb82977446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d870b7e0e611f7badf7cab8352fda7d",
+    "url": "/samarth_sweets/index.html"
+  },
+  {
+    "revision": "fbc2723aeace0610bcf0",
+    "url": "/samarth_sweets/static/css/main.161e1496.chunk.css"
+  },
+  {
+    "revision": "717f57e616f7860520d4",
+    "url": "/samarth_sweets/static/js/2.c53530ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samarth_sweets/static/js/2.c53530ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc2723aeace0610bcf0",
+    "url": "/samarth_sweets/static/js/main.cf92b5f4.chunk.js"
+  },
+  {
+    "revision": "3d2580e9770005c3f4a2",
+    "url": "/samarth_sweets/static/js/runtime-main.7b929ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samarth_sweets/static/media/logo.5d5d9eef.svg"
+  }
+]);
